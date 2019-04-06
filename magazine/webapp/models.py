b87679db6ed6ae5c276828d6eceab29300009c44
pavel <zhpv07@gmail.com>
2019-04-06 12:25:48 -0400

    Добавлены модели, сделана миграция.

--- magazine/webapp/models.py
+++ magazine/webapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    created_at = models.DateField()
+    categories = models.ManyToManyField('Category', related_name='categories', blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Foto(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='product')
+    foto = models.ImageField(upload_to='fotos')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='products')
+    phone_number = models.CharField(max_length=20)
+    adress = models.CharField(max_length=255, null=True, blank=True)
+    comment = models.TextField(max_length=2000, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+
